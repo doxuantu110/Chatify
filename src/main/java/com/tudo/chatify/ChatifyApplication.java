bb8aba53211928188a65a2e57b24369116cef897
Do Xuan Tu <doxuantutlbt@gmail.com>
2026-03-15 19:37:37 +0700

    Initial commit

---
+++ src/main/java/com/tudo/chatify/ChatifyApplication.java
@@ -0,0 +1,14 @@
+package com.tudo.chatify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatifyApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ChatifyApplication.class, args);
+	}
+
+}
